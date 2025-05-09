b5ce7a7142e7699f3a976406064ec87c947447b0
Soyunasilla <105076969+Soyunasilla@users.noreply.github.com>
2025-05-09 15:06:49 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -6,6 +6,7 @@ import numpy as np
 st.title('Socialize your knowledge')
 st.write('Se analizara el desempeño de los empleados de la empresa Socialize your knowledge')
 
+from PIL import Image, ImageDraw, ImageFont
 image = Image.open('Streamlit.jpeg')
 draw = ImageDraw.Draw(image)
 font = ImageFont.load_default()
