d86f57ead02e485a9c7ba6cc2f9ab8c79ffbb4c3
Soyunasilla <105076969+Soyunasilla@users.noreply.github.com>
2025-05-09 14:51:39 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -6,7 +6,7 @@ import numpy as np
 st.title('Socialize your knowledge')
 st.write('Se analizara el desempeño de los empleados de la empresa Socialize your knowledge')
 
-image = Image.open('streamlit.jpeg')
+image = Image.open('Streamlit.jpeg')
 draw = ImageDraw.Draw(image)
 font = ImageFont.load_default()
 draw.text((10, 10), "Zombies y!", fill=(255, 0, 0), font=font)
