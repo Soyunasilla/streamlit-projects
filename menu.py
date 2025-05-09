a9cc1369157a646f4f401bb6d1babbb46f35de74
Soyunasilla <105076969+Soyunasilla@users.noreply.github.com>
2025-05-09 15:08:09 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -7,6 +7,10 @@ st.title('Socialize your knowledge')
 st.write('Se analizara el desempeño de los empleados de la empresa Socialize your knowledge')
 
 from PIL import Image, ImageDraw, ImageFont
+
 image = Image.open('Streamlit.jpeg')
 draw = ImageDraw.Draw(image)
 font = ImageFont.load_default()
+draw.text((10, 10), "Socialize your Knowledge!", fill=(255, 0, 0), font=font)
+
+st.image(image, caption='Socialize your Knowledge')
